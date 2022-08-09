b43fe78e4f63679b59a830f553e5c45959033aeb
earl-stephens <stephensearl@hotmail.com>
2022-08-08 22:07:17 -0600

    Add serialization to App1

--- src/application/App2.java
+++ src/application/App2.java
@@ -1,10 +1,9 @@
 package application;
 
-public class App {
+public class App2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("App2");
 	}
 
 }
